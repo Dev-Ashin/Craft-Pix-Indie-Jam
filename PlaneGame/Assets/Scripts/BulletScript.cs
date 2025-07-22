6f842858a6c8e46394cc856d94a266cee3c7dc62
Dev-Ashin <ashingamedev@gmail.com>
2025-07-22 18:25:31 -0400

    jilla julla

--- PlaneGame/Assets/Scripts/BulletScript.cs
+++ PlaneGame/Assets/Scripts/BulletScript.cs
@@ -6,6 +6,7 @@ public class BulletScript : MonoBehaviour
     public float damageRate;
     public float bulletSpeed;
     private Rigidbody2D rb;
+    private string boundary = "Boundary";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,5 +23,14 @@ public class BulletScript : MonoBehaviour
     private void FixedUpdate()
     {
         rb.linearVelocity = transform.up * damageRate;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided");
+        if (collision.collider.CompareTag(boundary))
+        {
+            Debug.Log("CollidedONboundaryyyyy");
+            Destroy(gameObject);
+        }
     }
 }
